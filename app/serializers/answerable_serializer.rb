f617cd9c2d6bcdafc445a0e745db1af4a248fe62
Marthyn Olthof <Marthyn@live.nl>
2013-10-27 02:39:11 +0100

    Timeagostring

--- app/serializers/answerable_serializer.rb
+++ app/serializers/answerable_serializer.rb
@@ -1,5 +1,11 @@
+
 class AnswerableSerializer < ActiveModel::Serializer
-  attributes :id, :category_name, :question, :time_limit
+  include ActionView::Helpers::DateHelper
+  attributes :id, :category_name, :question, :time_limit, :time_ago_string
+
+  def time_ago_string
+    time_ago_in_words(object.created_at)
+  end
 
   def category_name
     object.category.name
