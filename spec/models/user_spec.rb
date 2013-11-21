13beb80c74d8d08358a24c1bf6167ddd74682470
jurre <jurrestender@hotmail.com>
2013-11-21 13:19:59 -0800

    add spec for counting total points

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,10 @@ describe User do
     it { should have_many :requests }
     it { should have_many :points }
   end
+
+  it "should correctly count the total points" do
+    user = FactoryGirl.create(:user)
+    2.times { user.points.create(amount: 10) }
+    expect(user.total_points).to eq(20)
+  end
 end
