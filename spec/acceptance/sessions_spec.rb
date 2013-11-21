8d6753fec819237250fa8280c828eff62814cd07
jurre <jurrestender@hotmail.com>
2013-11-21 13:12:39 -0800

    clean up session spec (needs more cleaning)

--- spec/acceptance/sessions_spec.rb
+++ spec/acceptance/sessions_spec.rb
@@ -6,8 +6,7 @@ resource "Sessions" do
   post "/api/v1/users" do
     user = User.create(email: "Marthyn@live.nl")
     parameter :email, "The users email address", scope: :user
-    user_post = {}
-    user_post[:email] = user.email
+    user_post = { email: user.email }
     example "Logging in" do
       do_request(user: user_post)
     end
