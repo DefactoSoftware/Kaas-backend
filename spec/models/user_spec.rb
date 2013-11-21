f7bfb134cdd4666f4ae2192af16a59dcfb62f286
jurre <jurrestender@hotmail.com>
2013-11-21 13:28:25 -0800

    add spec for when no points are given yet

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,9 +9,16 @@ describe User do
     it { should have_many :points }
   end
 
-  it "should correctly count the total points" do
-    user = FactoryGirl.create(:user)
-    2.times { user.points.create(amount: 10) }
-    expect(user.total_points).to eq(20)
+  describe "counting points" do
+    it "should be correct when points given" do
+      user = FactoryGirl.create(:user)
+      2.times { user.points.create(amount: 10) }
+      expect(user.total_points).to eq(20)
+    end
+
+    it "should be 0 when no points given" do
+      user = FactoryGirl.create(:user)
+      expect(user.total_points).to eq(0)
+    end
   end
 end
