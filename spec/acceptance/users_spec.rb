f3f5855a508e7240702060543e1fd92e36c07dd8
jurre <jurrestender@hotmail.com>
2013-11-21 13:10:50 -0800

    add spec for users#show

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -2,17 +2,22 @@ require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "Users" do
-
-  user = FactoryGirl.create(:user)
+  let(:user) { FactoryGirl.create(:user) }
+  let(:id) { user.id }
 
   post "/api/v1/users" do
     parameter :email, "The users email address", scope: :user
     parameter :name, "The users password", scope: :user
-    user_post = {}
-    user_post[:email] = user.email
-    user_post[:name] = user.name
+
+    let(:user_params) { { user: { email: user.email, name: user.name } } }
     example "Creating a user" do
-      do_request(user: user_post)
+      do_request(user: user_params)
+    end
+  end
+
+  get "/api/v1/users/:id" do
+    example_request "Getting a user" do
+      expect(response_status).to eq(200)
     end
   end
 end
